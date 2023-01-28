d60d72f98ca16a4f1f2be4d4f54ac5b75f237c5c
Brijbhna Chauhan <brijphy@gmail.com>
2023-01-28 23:36:12 +0530

    schedule cricket ui chnage

--- src/Components/MatchSchdule/index.jsx
+++ src/Components/MatchSchdule/index.jsx
@@ -9,17 +9,43 @@ const MatchSchedule = (props) => {
 
         {
         	props?.data?.map((slide, i)=> {
+        		console.log('slide', slide)
         			return(
         				<>
 			<div className="blog-slider__item swiper-slide">
             <div className="blog-slider__img">
-              
-              <img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759872/kuldar-kalvik-799168-unsplash.jpg" alt="" />
+               <div className='d-flex' style={{ 
+               	flexDirection: "column",
+    			marginTop: "23px"
+               }}>
+                <img src={slide.img} style={{
+                  width: "100px",
+			    height: "100px",
+			    marginRight: "5px",
+			    flexDirection: "column",
+			    marginLeft: "100px"
+                }}></img>
+                <h4 style={{
+                  alignSelf: 'center',
+                  marginRight: 5,
+                  marginTop: 5
+                }}>
+                  V/S
+                </h4>
+                <img src={slide.img2} style={{
+                  width: "100px",
+				    height: "100px",
+				    marginRight: "5px",
+				    flexDirection: "column",
+				    marginLeft: "100px"
+                }}></img>
+                      </div>
+              {/*<img src="https://res.cloudinary.com/muhammederdem/image/upload/v1535759872/kuldar-kalvik-799168-unsplash.jpg" alt="" />*/}
             </div>
             <div className="blog-slider__content">
-              <span className="blog-slider__code">26 December 2019</span>
-              <div className="blog-slider__title">Lorem Ipsum Dolor</div>
-              <div className="blog-slider__text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae voluptate repellendus magni illo ea animi? </div>
+              <span className="blog-slider__code">{slide?.date_2}</span>
+              <div className="blog-slider__title">{slide?.heading}</div>
+              <div className="blog-slider__text">{slide?.player} </div>
               <a href="#" className="blog-slider__button">READ MORE</a>
             </div>
           </div>
